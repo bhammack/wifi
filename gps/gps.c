00fdff24c766750de8ae0ddac1503531336e9a90
Ben Hammack <hammack.b@gmail.com>
2016-12-15 22:46:04 +0000

    Working

--- gps/gps.c
+++ gps/gps.c
@@ -2,64 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <math.h>
-
 
 int main(int argc, char** argv) {
-	int rc;
-	struct timeval tv;
-	struct gps_data_t gps_data;
-	if ((rc = gps_open("localhost", "2947", &gps_data)) == -1) {
-		printf("code: %d, reason: %s\n", rc, gps_errstr(rc));
-		return EXIT_FAILURE;
+	struct gps_data_t gpsdata;
+	int scanning = 1;
+	int rv;
+	if (gps_open("localhost", "2947", &gpsdata) < 0) {
+		perror("gps_open()");
+		return 1;
 	}
-	gps_stream(&gps_data, WATCH_ENABLE | WATCH_JSON, NULL);
+	gps_stream(&gpsdata, WATCH_ENABLE | WATCH_JSON, NULL);
 
-	while (1) {
-		/* wait for 2 seconds to receive data */
-		if (gps_waiting (&gps_data, 2000000)) {
-			/* read data */
-			if ((rc = gps_read(&gps_data)) == -1) {
-				printf("error occured reading gps data. code: %d, reason: %s\n", rc, gps_errstr(rc));
+	while (scanning) {
+		if (gps_waiting(&gpsdata, 2000000)) {
+			rv = gps_read(&gpsdata);
+			if (rv < 0) {
+				perror("gps_read()");
+			} else if (rv == 0) {
+				printf("no data in socket...\n");
 			} else {
-				/* Display data from the GPS receiver. */
-				if ((gps_data.status == STATUS_FIX) && 
-					(gps_data.fix.mode == MODE_2D || gps_data.fix.mode == MODE_3D) &&
-					!isnan(gps_data.fix.latitude) && 
-					!isnan(gps_data.fix.longitude)) {
-						//gettimeofday(&tv, NULL); EDIT: tv.tv_sec isn't actually the timestamp!
-						printf("latitude: %f, longitude: %f, speed: %f, timestamp: %ld\n", gps_data.fix.latitude, gps_data.fix.longitude, gps_data.fix.speed, gps_data.fix.time); //EDIT: Replaced tv.tv_sec with gps_data.fix.time
+				if ((gpsdata.status == STATUS_FIX) && 
+					(gpsdata.fix.mode == MODE_2D || gpsdata.fix.mode == MODE_3D)) {
+					// We've got a valid fix on our location!
+					printf("lat: %f | lng: %f | sats_used: %d | stas_vis: %d\n", 
+					gpsdata.fix.latitude, gpsdata.fix.longitude,
+					gpsdata.satellites_used, gpsdata.satellites_visible);
 				} else {
-					printf("no GPS data available\n");
+					printf("Getting a fix on the position...\n");
 				}
 			}
+		} else {
+			scanning = 0;
 		}
-		sleep(3);
 	}
 
-	/* When you are done... */
-	gps_stream(&gps_data, WATCH_DISABLE, NULL);
-	gps_close (&gps_data);
+	gps_stream(&gpsdata, WATCH_DISABLE, NULL);
+	gps_close(&gpsdata);
 
-	return EXIT_SUCCESS;
+	return 0;
 }
-
-
-/*
-pi@raspberrypi:~/wifi/gps $ gdb gps.OCCURING
-Reading symbols from gps.o...(no debugging symbols found)...done.
-(gdb) run
-Starting program: /home/pi/wifi/gps/gps.o
-[Thread debugging using libthread_db enabled]
-Using host libthread_db library "/lib/arm-linux-gnueabihf/libthread_db.so.1".
-Program received signal SIGSEGV, Segmentation fault.
-0x76fa0a28 in gps_sock_open () from /usr/lib/arm-linux-gnueabihf/libgps.so.21
-(gdb)
-*/
-
-/*
-libgps: gps_sock_open(localhost, 2947)
-libgps: netlib_connectsock() returns socket on fd 3
-Segmentation fault
-*/
-

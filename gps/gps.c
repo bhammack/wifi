691a44e6a2975a8d6064fd46586eee709bf32529
Ben Hammack <hammack.b@gmail.com>
2016-12-15 20:18:26 +0000

    So somehow, using std=c__ breaks my code

--- gps/gps.c
+++ gps/gps.c
@@ -1,25 +1,47 @@
+#include <gps.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <gps.h>
-
+#include <math.h>
 
 
 int main(int argc, char** argv) {
-	printf("main()\n");
-	gps_enable_debug(1, stdout);
-	// WHY IS SEGFAULT OCCURING? I HAVEN'T DONE ANYTHING WRONG?!?!?!
-	struct gps_data_t gpsdata;
-
-	//sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
 	int rc;
-	if ((rc = gps_open("localhost", "2947", &gpsdata)) == -1) {
-    	printf("code: %d, reason: %s\n", rc, gps_errstr(rc));
-		return 1;
+	struct timeval tv;
+	struct gps_data_t gps_data;
+	if ((rc = gps_open("localhost", "2947", &gps_data)) == -1) {
+		printf("code: %d, reason: %s\n", rc, gps_errstr(rc));
+		return EXIT_FAILURE;
 	}
-	
-	printf("done!\n");
-	return 0;
+	gps_stream(&gps_data, WATCH_ENABLE | WATCH_JSON, NULL);
+
+	while (1) {
+		/* wait for 2 seconds to receive data */
+		if (gps_waiting (&gps_data, 2000000)) {
+			/* read data */
+			if ((rc = gps_read(&gps_data)) == -1) {
+				printf("error occured reading gps data. code: %d, reason: %s\n", rc, gps_errstr(rc));
+			} else {
+				/* Display data from the GPS receiver. */
+				if ((gps_data.status == STATUS_FIX) && 
+					(gps_data.fix.mode == MODE_2D || gps_data.fix.mode == MODE_3D) &&
+					!isnan(gps_data.fix.latitude) && 
+					!isnan(gps_data.fix.longitude)) {
+						//gettimeofday(&tv, NULL); EDIT: tv.tv_sec isn't actually the timestamp!
+						printf("latitude: %f, longitude: %f, speed: %f, timestamp: %ld\n", gps_data.fix.latitude, gps_data.fix.longitude, gps_data.fix.speed, gps_data.fix.time); //EDIT: Replaced tv.tv_sec with gps_data.fix.time
+				} else {
+					printf("no GPS data available\n");
+				}
+			}
+		}
+		sleep(3);
+	}
+
+	/* When you are done... */
+	gps_stream(&gps_data, WATCH_DISABLE, NULL);
+	gps_close (&gps_data);
+
+	return EXIT_SUCCESS;
 }
 
 
@@ -40,6 +62,4 @@ libgps: gps_sock_open(localhost, 2947)
 libgps: netlib_connectsock() returns socket on fd 3
 Segmentation fault
 */
-
-
 

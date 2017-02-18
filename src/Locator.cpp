ac9044ee31aa1ae05fb653e7f868622dc3bcb121
Ben Hammack <hammack.b@gmail.com>
2017-02-18 00:29:53 +0000

    Added circle intersection algo

--- src/Locator.cpp
+++ src/Locator.cpp
@@ -43,23 +43,22 @@ double radius(double decibels, double gigahertz) {
 class Point {	
 	private:
 		// TODO: Add the def for PI and EARTH_RADIUS_KM here.
-		double deg2rad(double deg) { return (deg * PI / 180); };
-		double rad2deg(double rad) { return (rad * 180 / PI); };
+		double deg2rad(double deg) const { return (deg * PI / 180); };
+		double rad2deg(double rad) const { return (rad * 180 / PI); };
 	public:
-		Point(double lat, double lng) {
-			latitude = lat;
-			longitude = lng;
-		};
+		Point(double lat, double lng) 
+		: latitude(lat), longitude(lng) {}
+		
 		double latitude;
 		double longitude;
 		// Haversine formula. Distance returned in meters.
 		// http://stackoverflow.com/questions/10198985/calculating-the-distance-between-2-latitudes-and-longitudes-that-are-saved-in-a
-		double distance_to(Point* other) {
+		double distance_to(const Point& other) const {
 			double lat1r, lon1r, lat2r, lon2r, u, v;
 			lat1r = deg2rad(latitude);
 			lon1r = deg2rad(longitude);
-			lat2r = deg2rad(other->latitude);
-			lon2r = deg2rad(other->longitude);
+			lat2r = deg2rad(other.latitude);
+			lon2r = deg2rad(other.longitude);
 			u = sin((lat2r - lat1r)/2);
 			v = sin((lon2r - lon1r)/2);
 			return 2000.0 * EARTH_RADIUS_KM * asin(sqrt(u * u + cos(lat1r) * cos(lat2r) * v * v));
@@ -68,14 +67,47 @@ class Point {
 
 // A circle. Defined as a point, and a radius.
 class Circle {
-	private:
-		Point center;
-		double radius; // meters
+	public:
+		const Point center;
+		const double radius; // meters
 	public:
 		Circle(double lat, double lng, double rad)
-		: center(lat,lng) {
-			radius = rad;
+		: center(lat,lng), radius(rad) {}
+		
+		
+		int intersects(const Circle& other, Point* p) {
+			double d = center.distance_to(other.center);
+			if (d > (radius + other.radius)) {
+				// no solutions. circles don't touch.
+				double delta = d - (radius + other.radius);
+				fprintf(stderr, "Point::intersects(): circles are %f meters from intersection\n", delta);
+				return 1;
+			}
+			if (d < abs(radius - other.radius)) {
+				// no solutions. circles coincide.
+				fprintf(stderr, "Point::intersects(): circles coincide\n");
+				return 1;
+			}
+			if (d == 0) {
+				// infinite solutions.
+				fprintf(stderr, "Point::intersects(): infinite solutions\n");
+				return 1;
+			}
+			double a = ( pow(radius,2) - pow(other.radius,2) + pow(d,2)) / (2 * d);
+			double h = sqrt(pow(radius,2)-pow(a,2));
+			double p2lat = center.latitude + a*(other.center.latitude - center.latitude)/d;
+			double p2lng = center.longitude + a*(other.center.longitude - center.longitude)/d;
+			
+			double p3lng = p2lng + -1*h*(other.center.latitude - center.latitude)/d;
+			double p3lat = p2lat - h*(other.center.longitude - center.longitude)/d;
+			p->latitude = p3lat;
+			p->longitude = p3lng;
+			
+			return 0;
+			
+			//return Point(p3lat, p3lng);
 		};
+		
 };
 
 
@@ -152,9 +184,9 @@ static int cb_macs(void* macs_vector, int argc, char** argv, char** col_name) {
 
 // This here is what makes the whole project chooch.
 void Locator::locate_mac(std::string mac) {
-	//std::string select_data = "SELECT scans.id, scans.latitude, scans.longitude, \
-	scans.latitude_error, scans.longitude_error, data.signal, data.noise, \
-	data.quality FROM scans, data WHERE scans.id = data.scan_id AND data.mac = '" + mac + "';";
+	//std::string select_data = "SELECT scans.id, scans.latitude, scans.longitude,
+	//scans.latitude_error, scans.longitude_error, data.signal, data.noise,
+	//data.quality FROM scans, data WHERE scans.id = data.scan_id AND data.mac = '" + mac + "';";
 	
 	std::string select_data = "SELECT scans.id, scans.latitude, scans.longitude, scans.latitude_error, scans.longitude_error, data.signal, routers.frequency FROM scans, data, routers WHERE scans.id = data.scan_id AND routers.mac = data.mac AND data.mac = '" + mac + "';";
 	
@@ -188,12 +220,26 @@ void Locator::locate_mac(std::string mac) {
 
 
 void Locator::locate() {
+	Circle x (29.623988, -82.360681, 60.0);
+	Circle y (29.624230, -82.360083, 5.0);
+	Point i (0,0);
+	int retval = x.intersects(y, &i);
+	if (retval == 0) {
+		printf("the isection is at point %f, %f\n", i.latitude, i.longitude);
+	}
+	printf("retval: %d\n", retval);
+	
+	
+	
+	/*
+	
 	std::string select_macs = "SELECT mac FROM routers;";
 	std::vector<std::string> macs;
 	rv = sqlite3_exec(db, select_macs.c_str(), cb_macs, &macs, &errmsg);
 	for (unsigned int i = 0; i < macs.size(); i++) {
 		locate_mac(macs.at(i));
 	}
+	*/
 }
 
 
